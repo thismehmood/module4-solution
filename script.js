30767c060c5aac9761bbe46f664b00fcce14b000
tipu <this.tipu@gmail.com>
2018-09-27 11:51:15 -0700

    my module

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var 	ip = [prompt("Please enter and input")];
+var ip = [prompt("Please enter and input")];
 //var name2=["Waqar" , "Mehmood", "Junaid", "jbbar"];
 
 for(var name in ip){
@@ -13,7 +13,40 @@ for(var name in ip){
    }
    
 }
+/* 	*/
 
+(function (window) {
+	var helloSpeaker = {};
+	var Hello = "Hello";
+	helloSpeaker.speak = function (names) {
+  console.log(Hello + " " + names);
+}
+
+window.helloSpeaker = helloSpeaker;
+})(window);
+
+(function (window) {
+
+var speakWord = "Good Bye";
+var byeSpeaker = {};
+byeSpeaker.speak = function (names) {
+  console.log(speakWord + " " + names);
+}
+
+window.byeSpeaker = byeSpeaker;
+})(window);
+
+(function (window) {
+	var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+	for(var i = 0; i < names.length; i++){
+	  var firstLetter = names[i].charAt(0).toLowerCase();
+       if (firstLetter === 'j') {
+        byeSpeaker.speak(names[i]);
+  } else {
+    helloSpeaker.speak(names[i]);
+  }
+}
+})(window);
 
 console.log("------------------")
 console.log(ip); 
